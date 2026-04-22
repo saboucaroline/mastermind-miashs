1c004ecd1062a0ad69e5a08eb8b7868d0c4627d8
sylvana235 <guidigansylvana@gmail.com>
2026-04-22 22:36:02 +0200

    sy.py

--- Mode.py
+++ Mode.py
@@ -1,1 +1,9 @@
-print("mode")
+import random 
+COULEURS = ["V","B","J","O","M","R"]
+TAILLE_COMBINAISON = 4
+NBS_ESSAIS = 10 
+def generer_combinaison_secrete():
+    return random.choices(COULEURS,TAILLE_COMBINAISON)
+def obtrnir_tentative_joueur():
+    while True:
+        tantative=input()
